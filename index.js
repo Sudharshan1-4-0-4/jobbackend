b60ef25cc4b9043499f98c040bf0fbaa977dd64a
sudharshan <siddumothukuru@gmail.com>
2024-05-03 15:25:25 +0000

    first commt

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const dbPath = path.join(__dirname, "user_information.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3001, () => {
+      console.log("Server Running at http://localhost:3001/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+//Registe_API!!!
+
+app.post("/register", async (request, response) => {
+  const { name, phone, email, password } = request.body;
+  const hashedPassword = await bcrypt.hash(request.body.password, 10);
+  const selectUserQuery = `SELECT * FROM user WHERE name = '${name}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    const createUserQuery = `
+      INSERT INTO 
+        user (name, phone, email, password) 
+      VALUES 
+        (
+          
+          '${name}',
+          '${phone}',
+          '${email}',
+          '${hashedPassword}'
+          
+        )`;
+    const dbResponse = await db.run(createUserQuery);
+    const newUserId = dbResponse.lastID;
+    response.send(`Created new user with ${newUserId}`);
+  } else {
+    response.status = 400;
+    response.send("User already exists");
+  }
+});
+
+//Login_API!!!!
+
+app.post("/login", async (request, response) => {
+  const { name, password } = request.body;
+  const selectUserQuery = `SELECT * FROM user WHERE name = '${name}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+      response.send("Login Success!");
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
+  }
+});
+
+app.get("/user/", async (request, response) => {
+  const getUsersQuery = `
+  SELECT
+    *
+  FROM
+    user
+  ORDER BY
+    id;`;
+  const usersArray = await db.all(getUsersQuery);
+  response.send(usersArray);
+});
+
+app.get("/user1/", (request, response) => {
+  response.send("sudharshan");
+});
